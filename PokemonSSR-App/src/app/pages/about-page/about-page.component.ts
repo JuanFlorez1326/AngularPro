710f5ea12abea5b6445a5931069406731ba02e17
Juan Forez <juanks339@gmail.com>
2025-06-22 11:56:47 -0500

    new: ssr

--- PokemonSSR-App/src/app/pages/about-page/about-page.component.ts
+++ PokemonSSR-App/src/app/pages/about-page/about-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-page',
@@ -6,4 +7,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './about-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class AboutPageComponent { }
+export default class AboutPageComponent { 
+  private title = inject(Title);
+  private meta = inject(Meta);
+
+  ngOnInit(): void {
+    this.title.setTitle('About Page');
+    this.meta.updateTag({ name: 'description', content: 'This is the about page of our application.' });
+    this.meta.updateTag({ name: 'og: title', content: 'About Page' });
+    this.meta.updateTag({ name: 'keywords', content: 'Hola Mundo,Juan,Angular,Pokemon' });
+  }
+}
--- PokemonSSR-App/src/app/pages/pricing-page/pricing-page.component.ts
+++ PokemonSSR-App/src/app/pages/pricing-page/pricing-page.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, PLATFORM_ID } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pricing-page',
@@ -6,4 +8,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './pricing-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PricingPageComponent { }
+export default class PricingPageComponent { 
+  private title = inject(Title);
+  private meta = inject(Meta);
+  private platform = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    this.title.setTitle('Pricing Page');
+    this.meta.updateTag({ name: 'description', content: 'This is the pricing page of our application.' });
+    this.meta.updateTag({ name: 'og: title', content: 'Pricing Page' });
+    this.meta.updateTag({ name: 'keywords', content: 'Hola Mundo,Juan,Angular,Pokemon' });
+  }
+}
