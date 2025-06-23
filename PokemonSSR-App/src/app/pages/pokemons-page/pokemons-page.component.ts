12367f08a3a149a67a392c9f181ccb3d8a8489be
Juan Forez <juanks339@gmail.com>
2025-06-23 12:10:46 -0500

    new: pagination to pokemons

--- PokemonSSR-App/src/app/pages/pokemons-page/pokemons-page.component.ts
+++ PokemonSSR-App/src/app/pages/pokemons-page/pokemons-page.component.ts
@@ -3,25 +3,46 @@ import { PokemonListComponent } from "../../pokemons/components/pokemon-list/pok
 import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { SimplePokemon } from '../../pokemons/interfaces';
+import { ActivatedRoute, Router } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, tap } from 'rxjs';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pokemons-page',
-  imports: [PokemonListComponent, PokemonListSkeletonComponent],
+  imports: [
+    PokemonListComponent, 
+    // PokemonListSkeletonComponent
+  ],
   templateUrl: './pokemons-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class PokemonsPageComponent { 
 
+  private title = inject(Title);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
   private pokemonsService = inject(PokemonsService);
+
   public pokemons = signal<SimplePokemon[]>([]);
+  public currentPage = toSignal<number>(
+    this.route.queryParamMap.pipe(
+      map( params => params.get('page') ?? '1'),
+      map( page => (isNaN(+page) ? 1 : +page)),
+      map( page => Math.max(1, page))
+    )
+  )
 
   ngOnInit(): void {
     this.loadPokemons();
   }
 
-  public loadPokemons( nextPage = 0 ) {
-    this.pokemonsService.loadPage(nextPage).subscribe( pokemons => {
-      this.pokemons.set(pokemons);
-    })
+  public loadPokemons( page = 0 ) {
+    const pageToLoad = this.currentPage()! + page;
+
+    this.pokemonsService.loadPage(pageToLoad).pipe(
+      tap(() => this.router.navigate([], { queryParams: { page: pageToLoad }})),
+      tap(() => this.title.setTitle(`Pokemons SSR - Page ${pageToLoad}`))
+    ).subscribe( pokemons => {  this.pokemons.set(pokemons) });
   }
 }
