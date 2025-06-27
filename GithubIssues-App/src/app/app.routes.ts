3b6df9c8180e3e668ee176e49e2df0915d529d50
Juan Forez <juanks339@gmail.com>
2025-06-26 19:45:23 -0500

    new: folder structure

--- GithubIssues-App/src/app/app.routes.ts
+++ GithubIssues-App/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'issue/:number',
+        loadComponent: () => import('./features/issues/pages/issue/issue-page.component'),
+    },
+    {
+        path: 'issues',
+        loadComponent: () => import('./features/issues/pages/issues-list/issues-list-page.component'),
+    },
+    {
+        path: '**',
+        redirectTo: 'issues',
+    },
+];
