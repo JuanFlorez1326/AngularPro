6d418143a505b3fc5546f6e5ced7c3528d6a3b29
Juan Forez <juanks339@gmail.com>
2025-06-27 19:35:34 -0500

    new: added TanStack Query

--- GithubIssues-App/src/app/features/issues/pages/issues-list/issues-list-page.component.ts
+++ GithubIssues-App/src/app/features/issues/pages/issues-list/issues-list-page.component.ts
@@ -1,9 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IssuesService } from '../../services/issues.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-issues-list-page',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './issues-list-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class IssuesListPageComponent { }
+export default class IssuesListPageComponent { 
+
+  private issuesService = inject(IssuesService);
+
+  get labelsQuery() {
+    return this.issuesService.labelsQuery;
+  }
+}
