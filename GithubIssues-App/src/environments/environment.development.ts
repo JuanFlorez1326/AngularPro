500e064eb9085588ddc2ebbd49869e7e97f7c94c
Juan Forez <juanks339@gmail.com>
2025-07-01 13:38:48 -0500

    delete: remove environments

--- GithubIssues-App/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    baseUrl: 'https://api.github.com/repos/angular/angular',
-    githubToken: ''
-    // githubToken: 'https://github.com/settings/personal-access-tokens'
-};
