65d6052e1fabefa065b4e7ae5d102f0907502ace
Juan Forez <juanks339@gmail.com>
2025-07-09 17:57:58 -0500

    new: remove project

--- Company-App/src/app/modules/admin/pages/summary/summary.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-summary',
-  imports: [],
-  templateUrl: './summary.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export default class SummaryComponent { }
